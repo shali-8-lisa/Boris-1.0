f0f5c51831cffd40e0f70daebfc8d8e3ff5790f1
lisa <shali-8-lisa@gmail.com>
2017-02-20 11:00:58 +0000

    Boris keyboard movement

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public float speedR;
 
 	private Rigidbody rb;
 
@@ -15,7 +16,13 @@ public class PlayerController : MonoBehaviour {
 
 	void FixedUpdate ()
 	{
+		//movement - keyboard
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+		transform.Translate (moveHorizontal * Time.deltaTime * speed, 0.0f, moveVertical * Time.deltaTime * speed);
+
+		//rotation - mouse
+
+
 	}
 }
