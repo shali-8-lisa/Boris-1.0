8d0b283c8bee106975d8ab361cb7bea2205942bc
lisa <shali-8-lisa@gmail.com>
2017-02-23 13:02:59 +0000

    Smooth the paths

--- Assets/scripts/Pathfinding/Unit.cs
+++ Assets/scripts/Pathfinding/Unit.cs
@@ -4,42 +4,92 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour {
 
+	const float minPathUpdateTime = 0.2f;
+	const float pathUpdateMoveThreshold = 0.5f;
+
 	public Transform target;
-	float speed = 15;
-	Vector3[] path;
-	int targetIndex;
+	public float speed = 15;
+	public float turnSpeed = 3;
+	public float turnDst = 5;
+	public float stoppingDst = 10;
+
+	Path path;
 
 	void Start() 
 	{
-		PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+		StartCoroutine (UpdatePath ());
 	}
 
-	public void OnPathFound(Vector3[] newPath, bool pathSuccessful) 
+	public void OnPathFound(Vector3[] waypoints, bool pathSuccessful) 
 	{
 		if (pathSuccessful) 
 		{
-			path = newPath;
-			targetIndex = 0;
+			path = new Path(waypoints, transform.position, turnDst, stoppingDst);
+
 			StopCoroutine("FollowPath");
 			StartCoroutine("FollowPath");
 		}
 	}
 
-	IEnumerator FollowPath() 
+	//follow Boris when he moves
+	IEnumerator UpdatePath() 
 	{
-		Vector3 currentWaypoint = path[0];
+		if (Time.timeSinceLevelLoad < 0.3f) 
+			yield return new WaitForSeconds (0.3f);
+
+		PathRequestManager.RequestPath (transform.position, target.position, OnPathFound);
+
+		float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+		Vector3 targetPosOld = target.position;
 
 		while (true) 
 		{
-			if (transform.position == currentWaypoint) 
+			yield return new WaitForSeconds (minPathUpdateTime);
+			if ((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold) 
 			{
-				targetIndex ++;
-				if (targetIndex >= path.Length)
-					yield break;
-				currentWaypoint = path[targetIndex];
+				PathRequestManager.RequestPath (transform.position, target.position, OnPathFound);
+				targetPosOld = target.position;
+			}
+		}
+	}
+
+	IEnumerator FollowPath() 
+	{
+		bool followingPath = true;
+		int pathIndex = 0;
+		transform.LookAt (path.lookPoints [0]);
+
+		float speedPercent = 1;
+
+		while (followingPath) 
+		{
+			Vector2 pos2D = new Vector2 (transform.position.x, transform.position.z);
+			while (path.turnBoundaries [pathIndex].HasCrossedLine (pos2D)) 
+			{
+				if (pathIndex == path.finishLineIndex) 
+				{
+					followingPath = false;
+					break;
+				} 
+				else 
+					pathIndex++;
 			}
 
-			transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+			if (followingPath) 
+			{
+				//slow down when gets close
+				if (pathIndex >= path.slowDownIndex && stoppingDst > 0) 
+				{
+					speedPercent = Mathf.Clamp01 (path.turnBoundaries [path.finishLineIndex].DistanceFromPoint (pos2D) / stoppingDst);
+					if (speedPercent < 0.01f)
+						followingPath = false;
+				}
+
+				Quaternion targetRotation = Quaternion.LookRotation (path.lookPoints [pathIndex] - transform.position);
+				transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+				transform.Translate (Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+			}
+
 			yield return null;
 		}
 	}
@@ -47,17 +97,6 @@ public class Unit : MonoBehaviour {
 	public void OnDrawGizmos() 
 	{
 		if (path != null) 
-		{
-			for (int i = targetIndex; i < path.Length; i++) 
-			{
-				Gizmos.color = Color.yellow;
-				Gizmos.DrawCube(path[i], Vector3.one);
-
-				if (i == targetIndex)
-					Gizmos.DrawLine(transform.position, path[i]);
-				else
-					Gizmos.DrawLine(path[i-1], path[i]);
-			}
-		}
+			path.DrawWithGizmos ();
 	}
 }
