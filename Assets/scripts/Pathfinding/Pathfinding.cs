8f83c986068a100cc56b932bfc832356d219ebff
lisa <shali-8-lisa@gmail.com>
2017-02-23 13:32:25 +0000

    Run pathfinding on a separate thread

--- Assets/scripts/Pathfinding/Pathfinding.cs
+++ Assets/scripts/Pathfinding/Pathfinding.cs
@@ -6,21 +6,14 @@ using System;
 
 public class Pathfinding : MonoBehaviour {
 
-	PathRequestManager requestManager;
 	Grid grid;
 
 	void Awake() 
 	{
-		requestManager = GetComponent<PathRequestManager> ();
 		grid = GetComponent<Grid> ();
 	}
 
-	public void StartFindPath (Vector3 startPos, Vector3 targetPos)
-	{
-		StartCoroutine (FindPath(startPos, targetPos));
-	}
-
-	IEnumerator FindPath(Vector3 startPos, Vector3 targetPos) 
+	public void FindPath(PathRequest request, Action<PathResult> callback) 
 	{
 		Stopwatch sw = new Stopwatch ();
 		sw.Start ();
@@ -28,8 +21,8 @@ public class Pathfinding : MonoBehaviour {
 		Vector3[] waypoints = new Vector3[0];
 		bool pathSuccess = false;
 
-		Node startNode = grid.NodeFromWorldPoint(startPos);
-		Node targetNode = grid.NodeFromWorldPoint(targetPos);
+		Node startNode = grid.NodeFromWorldPoint(request.pathStart);
+		Node targetNode = grid.NodeFromWorldPoint(request.pathEnd);
 
 		if (startNode.walkable && targetNode.walkable) 
 		{
@@ -48,7 +41,7 @@ public class Pathfinding : MonoBehaviour {
 
 				if (node == targetNode) {
 					sw.Stop ();
-					print ("Path found: " + sw.ElapsedMilliseconds + "ms");
+					//print ("Path found: " + sw.ElapsedMilliseconds + "ms");
 					pathSuccess = true;
 					break;
 				}
@@ -81,12 +74,14 @@ public class Pathfinding : MonoBehaviour {
 				}
 			}
 		}
-		yield return null;
 
-		if (pathSuccess)
-			waypoints = RetracePath(startNode,targetNode);
+		if (pathSuccess) 
+		{
+			waypoints = RetracePath (startNode, targetNode);
+			pathSuccess = waypoints.Length > 0;
+		}
 
-		requestManager.FinishedProcessingPath(waypoints, pathSuccess);
+		callback (new PathResult (waypoints, pathSuccess, request.callback));
 
 	}
 
--- Assets/scripts/Pathfinding/Unit.cs
+++ Assets/scripts/Pathfinding/Unit.cs
@@ -37,7 +37,7 @@ public class Unit : MonoBehaviour {
 		if (Time.timeSinceLevelLoad < 0.3f) 
 			yield return new WaitForSeconds (0.3f);
 
-		PathRequestManager.RequestPath (transform.position, target.position, OnPathFound);
+		PathRequestManager.RequestPath (new PathRequest(transform.position, target.position, OnPathFound));
 
 		float sqrMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
 		Vector3 targetPosOld = target.position;
@@ -47,7 +47,7 @@ public class Unit : MonoBehaviour {
 			yield return new WaitForSeconds (minPathUpdateTime);
 			if ((target.position - targetPosOld).sqrMagnitude > sqrMoveThreshold) 
 			{
-				PathRequestManager.RequestPath (transform.position, target.position, OnPathFound);
+				PathRequestManager.RequestPath (new PathRequest(transform.position, target.position, OnPathFound));
 				targetPosOld = target.position;
 			}
 		}
